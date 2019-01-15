7265420ba2649d6273d9fb59216d18af0b2f64c4
Caitlin Cooling <ccooling97@gmail.com>
2019-01-15 19:13:30 +0000

    Handling edge case: when the history contains a duplicate entry
    - As this effects the number that is used to calculate the average, if an entry was entered twice on the same day, it is removed

--- spec/annualMileageSpec.js
+++ spec/annualMileageSpec.js
@@ -47,4 +47,32 @@ describe("calculateMileage", function() {
      }]
     expect(calculateMileage(history)).toBe(7702.666666666667)
   });
+
+  it("gets the average mileage when given a history with duplicate events", function() {
+    const history = [{
+      event: 'MOT',
+      date: '2018-3-10T00:00:00.000Z',
+      data: {
+        mileage: 69383,
+        passed: true
+      }
+    },
+    {
+      event: 'MOT',
+      date: '2017-3-10T00:00:00.000Z',
+      data: {
+        mileage: 58385,
+        passed: true
+      }
+    },
+    {
+      event: 'MOT',
+      date: '2017-3-10T00:00:00.000Z',
+      data: {
+        mileage: 58385,
+        passed: true
+       }
+     }]
+    expect(calculateMileage(history)).toBe(5499)
+  });
 });
--- src/annualMileage.js
+++ src/annualMileage.js
@@ -3,6 +3,12 @@ let calculateMileage = (history) => {
   let averageMileage = 0;
   let totalChange = 0;
 
+  for(let i = 0; i < history.length-1; i++){
+    if(history[i].date === history[i + 1].date) {
+      years -= 1;
+    }
+  }
+
   totalChange = history[0].data.mileage - history[history.length-1].data.mileage;
   return averageMileage = totalChange / years
 };
