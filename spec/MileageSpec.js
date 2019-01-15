5c7b826dac45865d5f0b05070756742bdb60151d
Caitlin Cooling <ccooling97@gmail.com>
2019-01-15 19:26:59 +0000

    Refactor:
    - Added Mileage class in preparation for the next questions, as the 
    calculateMileage method will be needed
    - Changed files names accordingly

---
+++ spec/MileageSpec.js
@@ -0,0 +1,139 @@
+describe("Mileage", function() {
+  let mileage;
+
+  beforeEach(function(){
+    mileage = new Mileage();
+  });
+
+  describe("calculateMileage", function() {
+    it("gets the average mileage when given a history with 2 events", function() {
+      const history = [{
+        event: 'MOT',
+        date: '2018-3-10T00:00:00.000Z',
+        data: {
+          mileage: 69383,
+          passed: true
+        }
+      },
+      {
+        event: 'MOT',
+        date: '2017-3-10T00:00:00.000Z',
+        data: {
+          mileage: 58385,
+          passed: true
+        }
+      }]
+      expect(mileage.calculateMileage(history)).toBe(5499)
+    });
+
+    it("gets the average mileage when given a history with 3 events", function() {
+      const history = [{
+        event: 'MOT',
+        date: '2018-3-10T00:00:00.000Z',
+        data: {
+          mileage: 69383,
+          passed: true
+        }
+      },
+      {
+        event: 'MOT',
+        date: '2017-3-10T00:00:00.000Z',
+        data: {
+          mileage: 58385,
+          passed: true
+        }
+      },
+      {
+         event: 'MOT',
+         date: '2016-4-10T00:00:00.000Z',
+         data: {
+           mileage: 46275,
+           passed: true
+         }
+       }]
+      expect(mileage.calculateMileage(history)).toBe(7702.666666666667)
+    });
+
+    it("gets the average mileage when given a history with duplicate events", function() {
+      const history = [{
+        event: 'MOT',
+        date: '2018-3-10T00:00:00.000Z',
+        data: {
+          mileage: 69383,
+          passed: true
+        }
+      },
+      {
+        event: 'MOT',
+        date: '2017-3-10T00:00:00.000Z',
+        data: {
+          mileage: 58385,
+          passed: true
+        }
+      },
+      {
+        event: 'MOT',
+        date: '2017-3-10T00:00:00.000Z',
+        data: {
+          mileage: 58385,
+          passed: true
+         }
+       }]
+      expect(mileage.calculateMileage(history)).toBe(5499)
+    });
+
+    it("gets the average mileage when given a test history", function () {
+      const history = [
+       {
+         event: 'MOT',
+         date: '2018-3-10T00:00:00.000Z',
+         data: {
+           mileage: 69383,
+           passed: true
+         }
+       },
+       {
+         event: 'MOT',
+         date: '2017-3-10T00:00:00.000Z',
+         data: {
+           mileage: 58385,
+           passed: true
+         }
+       },
+       {
+         event: 'MOT',
+         date: '2016-4-10T00:00:00.000Z',
+         data: {
+           mileage: 46275,
+           passed: true
+         }
+       },
+       {
+         event: 'MOT',
+         date: '2016-4-10T00:00:00.000Z',
+         data: {
+           mileage: 46275,
+           passed: false
+         }
+       },
+       {
+         event: 'MOT',
+         date: '2015-4-10T00:00:00.000Z',
+         data: {
+           mileage: 37375,
+           passed: true
+         }
+       },
+       {
+         event: 'MOT',
+         date: '2014-4-10T00:00:00.000Z',
+         data: {
+           mileage: 37375,
+           passed: true
+         }
+       }
+      ]
+      expect(mileage.calculateMileage(history)).toBe(6401.6)
+    });
+  });
+})
