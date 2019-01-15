73a709aa967702e7cf91ca9f5bf19faf3168f73a
Caitlin Cooling <ccooling97@gmail.com>
2019-01-15 19:00:50 +0000

    Average mileage is calculated when given a history array with 2 events

--- spec/annualMileageSpec.js
+++ spec/annualMileageSpec.js
@@ -1,1 +1,22 @@
+describe("calculateMileage", function() {
 
+  it("gets the average mileage when given a history with 2 events", function() {
+    const history = [{
+      event: 'MOT',
+      date: '2018-3-10T00:00:00.000Z',
+      data: {
+        mileage: 69383,
+        passed: true
+      }
+    },
+    {
+      event: 'MOT',
+      date: '2017-3-10T00:00:00.000Z',
+      data: {
+        mileage: 58385,
+        passed: true
+      }
+    }]
+    expect(calculateMileage(history)).toBe(5499)
+  });
+});
--- src/annualMileage.js
+++ src/annualMileage.js
@@ -1,1 +1,8 @@
+let calculateMileage = (history) => {
+  let years = history.length;
+  let averageMileage = 0;
+  let totalChange = 0;
 
+  totalChange = history[0].data.mileage - history[history.length-1].data.mileage;
+  return averageMileage = totalChange / years
+};
