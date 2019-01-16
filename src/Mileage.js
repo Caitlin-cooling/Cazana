8d079ad5f0e5e03a19f8711882dbeda58d0e8947
Caitlin Cooling <ccooling97@gmail.com>
2019-01-16 12:50:12 +0000

    File now passes linter

--- src/Mileage.js
+++ src/Mileage.js
@@ -1,52 +1,52 @@
 class Mileage {
-  constructor(history, date) {
-    this.history = history;
-    this.averageMileage;
-    this.today = date || new Date();
+  constructor (history, date) {
+    this.history = history
+    this.averageMileage = 0
+    this.today = date || new Date()
   }
 
-  calculateAnnualAverageMileage() {
-    let years = this.history.length;
-    let averageMileage = 0;
-    let totalChange = 0;
+  calculateAnnualAverageMileage () {
+    let years = this.history.length
+    let totalChange = 0
 
-    for(let i = 0; i < this.history.length-1; i++){
-      if(this.history[i].date === this.history[i + 1].date) {
-        years -= 1;
+    for (let i = 0; i < this.history.length - 1; i++) {
+      if (this.history[i].date === this.history[i + 1].date) {
+        years -= 1
       }
     }
 
-    totalChange = this.history[0].data.mileage - this.history[this.history.length-1].data.mileage;
-    return this.averageMileage = totalChange / years
+    totalChange = this.history[0].data.mileage - this.history[this.history.length - 1].data.mileage
+    this.averageMileage = totalChange / years
+    return this.averageMileage
   };
 
-  currentMileage() {
-    let mileagePerDay = 0;
-    let daysDifference = 0;
-    let totalMileage = 0;
-    let formattedDate = this.formatDate(this.history[0].date);
-    let lastMOTDate = new Date(formattedDate);
+  currentMileage () {
+    let mileagePerDay = 0
+    let daysDifference = 0
+    let totalMileage = 0
+    let formattedDate = this.formatDate(this.history[0].date)
+    let lastMOTDate = new Date(formattedDate)
 
-    this.calculateAnnualAverageMileage();
-    mileagePerDay = this.averageMileage / 365;
+    this.calculateAnnualAverageMileage()
+    mileagePerDay = this.averageMileage / 365
 
-    daysDifference = (this.today - lastMOTDate) / (1000 * 60 * 60 * 24);
-    totalMileage = this.history[0].data.mileage + (daysDifference * mileagePerDay);
-    return totalMileage;
+    daysDifference = (this.today - lastMOTDate) / (1000 * 60 * 60 * 24)
+    totalMileage = this.history[0].data.mileage + (daysDifference * mileagePerDay)
+    return totalMileage
   }
 
-  formatDate(date) {
-    let newDates = [];
-    let array = [];
-    let dateArray = [];
+  formatDate (date) {
+    let newDates = []
+    let array = []
+    let dateArray = []
 
     array = date.split('T')
     dateArray = array[0].split('-')
 
-    for(let i = 0; i < dateArray.length; i ++) {
+    for (let i = 0; i < dateArray.length; i++) {
       newDates.push(parseInt(dateArray[i]))
     }
 
-    return newDates;
+    return newDates
   }
 };
